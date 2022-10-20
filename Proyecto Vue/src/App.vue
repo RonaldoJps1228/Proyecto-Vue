f045e64cda5d43116458722dcf370ca0ae648c7a
RonaldoJps1228 <114714102+RonaldoJps1228@users.noreply.github.com>
2022-10-20 18:24:52 -0500

    Add files via upload

---
+++ Proyecto Vue/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Hola Mundo en Vue"/> -->
+  <!-- <hello-world msg="Hi my world in Vue"><hello-world/> -->
+
+  <!-- <Counter title="Entrada"/>
+  < Counter title="Salida"/> -->
+  <!-- <Counter  :start="101" /> -->
+  <!-- <Counter /> -->
+
+  <Indecision />
+
+</template>
+
+<script>
+
+  // import Counter from './components/Counter.vue';
+  import Indecision from './components/Indecision.vue';
+
+  export default {
+    name: "App",
+    components: { 
+      //Counter,
+      Indecision 
+    }
+  }
+</script>
+
+<style>
+  #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  }
+
+</style>
---
+++ Proyecto Vue/src/components/Counter.vue
@@ -0,0 +1,74 @@
+<template>
+  <h2>{{ customTitle }}</h2>
+  <p> {{ counter }} <sup>2</sup> = {{ squareCounter }} </p>
+
+  <div>
+    <button @click="getIncrease">+1</button>
+    <button @click="getDecrease">-1</button>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+    // props : ['title'],
+    // name: 'Counter'
+    props: { // Properties es util para las validaciones
+      title: String,
+      start: {
+          type: Number,
+          default: 100,
+          // required: true
+          validator(value) {
+            return value >= 100
+          }
+      }
+    },
+    data() {
+        return {
+        // counter: 5 // Contador
+        counter: this.start
+        }
+    },
+    methods: {
+      getSquareValue() {
+        return this.counter * this.counter
+      },
+      getIncrease() {
+        this.counter++
+      },
+      getDecrease() {
+        this.counter--
+      }
+    },
+    computed: { //Metodos computarizadas
+      squareCounter() {
+        return this.counter * this.counter
+      },
+      customTitle() {
+        return this.title || 'Counter'
+      }
+    }
+}
+</script>
+
+<style>
+button{
+  background-color: rgb(196, 78, 38);
+  border-radius: 5px;
+  border: 1px solid white;
+  color: blue;
+  cursor: pointer;
+  margin: auto;
+  padding: 5px 15px;
+  transition: 0.3s ease-in-out;
+
+}
+
+button:hover{
+  background-color: aquamarine;
+  transition: 0.3s ease-in-out;
+}
+
+</style>
